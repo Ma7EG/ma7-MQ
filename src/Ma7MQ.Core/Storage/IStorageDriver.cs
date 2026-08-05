e44895321e3290ca04e4449dbdf56d42af112493
Ma7EG <ma7@example.com>
2026-08-05 10:00:00 +0300

    feat: define storage driver interface

---
+++ src/Ma7MQ.Core/Storage/IStorageDriver.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ma7MQ.Core.Types;
+
+namespace Ma7MQ.Core.Storage
+{
+    public interface IStorageDriver
+    {
+        Task SaveMessageAsync(MQMessage msg);
+        Task<List<MQMessage>> GetMessagesAsync(string topic, int limit);
+        Task SaveTopicAsync(MQTopic topic);
+        Task RegisterConsumerAsync(string groupName, string consumerID);
+        Task UpdateHeartbeatAsync(string groupName, string consumerID);
+    }
+}
